a94012080f878cac7808aeae9fd85b94905be679
Alex Martins <alex.oliveiramartins05@gmail.com>
2026-06-03 12:07:10 -0300

    feat: remove ready check from handler

--- internal/app/handlers.go
+++ internal/app/handlers.go
@@ -1,9 +1,8 @@
 package app
 
 import (
-	"net"
+	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/bytedance/sonic"
 
@@ -48,21 +47,12 @@ func (a *App) FraudScoreHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func socketReady(path string) bool {
-	conn, err := net.DialTimeout("unix", path, 10*time.Millisecond)
-	if err != nil {
-		return false
-	}
-
-	_ = conn.Close()
-	return true
-}
-
 func (a *App) ReadyHandler(w http.ResponseWriter, r *http.Request) {
-	if !socketReady("/var/run/rinha/api1.sock") || !socketReady("/var/run/rinha/api2.sock") {
-		w.WriteHeader(http.StatusServiceUnavailable)
-		return
+	w.Header().Set("Content-Type", "application/json")
+	switch r.Method {
+	case http.MethodGet:
+		fmt.Fprintln(w, "Hello World")
+	default:
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 	}
-
-	w.WriteHeader(http.StatusOK)
 }
